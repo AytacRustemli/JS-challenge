068db4291fc7429f9350e976673a9a8dfa585fa0
AytacRustemli <aytacrustmli93@gmail.com>
2023-10-03 13:46:36 +0400

    SearchApp

--- Todo/js/main.js
+++ Todo/js/main.js
@@ -11,49 +11,49 @@ runEvents();
 
 function runEvents() {
     form.addEventListener("submit", addTodo);
-    document.addEventListener("DOMContentLoaded",pageLoaded);
-    secondCardBody.addEventListener("click",removeTodoToUI);
-    clearButton.addEventListener("click",allTodos);
-    filterInput.addEventListener("keyup",filter);
+    document.addEventListener("DOMContentLoaded", pageLoaded);
+    secondCardBody.addEventListener("click", removeTodoToUI);
+    clearButton.addEventListener("click", allTodos);
+    filterInput.addEventListener("keyup", filter);
 }
 
-function filter(e) { 
+function filter(e) {
     const filterValue = e.target.value.toLowerCase().trim();
     const todoListesi = document.querySelectorAll(".list-group-item");
     if (todoListesi.length > 0) {
-       todoListesi.forEach(function(todo) {
-        if (todo.textContent.toLowerCase().trim().includes(filterValue)) {
-            todo.setAttribute("style","display : block");
-        }
-        else{
-            todo.setAttribute("style","display : none !important");
-        }
-       })
+        todoListesi.forEach(function (todo) {
+            if (todo.textContent.toLowerCase().trim().includes(filterValue)) {
+                todo.setAttribute("style", "display : block");
+            }
+            else {
+                todo.setAttribute("style", "display : none !important");
+            }
+        })
     }
-    else{
-        showAlert("warning","Filtrelemek icin en az bir todo olmali.");
+    else {
+        showAlert("warning", "Filtrelemek icin en az bir todo olmali.");
     }
 }
 
 function allTodos() {
     const todoListesi = document.querySelectorAll(".list-group-item");
     if (todoListesi.length > 0) {
-        todoListesi.forEach(function(params) {
+        todoListesi.forEach(function (params) {
             params.remove();
         })
         todos = [];
-        localStorage.setItem("todos",JSON.stringify(todos));
-        showAlert("success","Basariyla silindi.");
+        localStorage.setItem("todos", JSON.stringify(todos));
+        showAlert("success", "Basariyla silindi.");
     }
-    else{
-        showAlert("warning","Silmek icin en az bir todo olmalidir.")
+    else {
+        showAlert("warning", "Silmek icin en az bir todo olmalidir.")
     }
-    
+
 }
 
 function pageLoaded() {
     checkTodosfromStorage();
-    todos.forEach(function(todo) {
+    todos.forEach(function (todo) {
         addTodoUI(todo);
     })
 }
@@ -63,29 +63,29 @@ function removeTodoToUI(e) {
         const todo = e.target.parentElement.parentElement;
         todo.remove();
         removeTodoToStorage(todo.textContent);
-        showAlert("success","Todo basariyla silindi..");
+        showAlert("success", "Todo basariyla silindi..");
     }
 }
 
-function removeTodoToStorage(removeTodo){
+function removeTodoToStorage(removeTodo) {
     checkTodosfromStorage();
-    todos.forEach(function(todo,index) {
+    todos.forEach(function (todo, index) {
         if (removeTodo === todo) {
-            todos.splice(index,1);
+            todos.splice(index, 1);
         }
     });
-    localStorage.setItem("todos",JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 function addTodo(e) {
     const inputText = addInput.value.trim();
     if (inputText == null || inputText == "") {
-        showAlert("warning","Lutfen bos birakmayiniz..");
+        showAlert("warning", "Lutfen bos birakmayiniz..");
     }
     else {
         addTodoUI(inputText);
         addTodoStorage(inputText);
-        showAlert("success","Todo eklendi");
+        showAlert("success", "Todo eklendi");
     }
     e.preventDefault();
 }
@@ -109,22 +109,22 @@ function addTodoUI(newTodo) {
     addInput.value = "";
 }
 
-function addTodoStorage(newTodo){
+function addTodoStorage(newTodo) {
     checkTodosfromStorage();
     todos.push(newTodo);
-    localStorage.setItem("todos",JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 function checkTodosfromStorage() {
     if (localStorage.getItem("todos") === null) {
         todos = [];
     }
-    else{
+    else {
         todos = JSON.parse(localStorage.getItem("todos"));
     }
 }
 
-function showAlert(type,message) {
+function showAlert(type, message) {
     const div = document.createElement("div");
     div.className = `alert alert-${type}`;
     div.textContent = message;
@@ -133,3 +133,75 @@ function showAlert(type,message) {
         div.remove();
     }, 2500);
 }
+
+
+
+// let check = true;
+// function createPromise(){
+//     return new Promise((resolve,reject) =>{
+//         if (check) {
+//             resolve("resolve oldu");
+//         }
+//         else{
+//             reject("olmadi");
+//         }
+//     })
+// }
+
+// createPromise()
+// .then((response) => {
+//     console.log(response);
+// })
+// .catch((error) => console.log(error))
+// .finally(() => console.log("her zaman isler"));
+
+// function readStudents(url){
+//     return new Promise((resolve,reject)=>{
+//         const xhr = new XMLHttpRequest();
+//         try{
+//             xhr.addEventListener("readystatechange", ()=>{
+//                 if(xhr.readyState === 4 && xhr.status === 200){
+//                     resolve(JSON.parse(xhr.responseText));
+//                 }
+//             })
+//         }
+//         catch(error){
+//             reject(error);
+//         }
+
+//         xhr.open("GET", url);
+//         xhr.send();
+//     })
+// }
+
+// readStudents("https://jsonplaceholder.typicode.com/posts").then((data) => console.log(data))
+// .catch((err) => console.log(err));
+
+// function getUsers(url) {
+//     return new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest();
+//         xhr.addEventListener("readystatechange", () => {
+//             try {
+//                 if (xhr.readyState === 4 && xhr.status === 200) {
+//                     resolve(JSON.parse(xhr.responseText));
+//                 }
+//             }
+//             catch (error) {
+//                 reject(error);
+//             }
+//         })
+
+//         xhr.open("GET", url);
+//         xhr.send();
+
+//     })
+// }
+
+// getUsers("https://jsonplaceholder.typicode.com/users")
+//     .then((data) => {
+//         console.log(data)
+//         data.forEach((user) =>
+//             console.log(user.name));
+//     }
+//     )
+//     .catch((err) => console.log(err));
